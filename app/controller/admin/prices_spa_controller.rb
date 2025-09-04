d176c4d310d98c62d088898dec170ae36dd77cd0
Raúl Martínez <rme@users.noreply.github.com>
2025-09-04 20:28:17 +0200

    simplified some initialization code on the prices spa and removed prices endpoint from samples

--- app/controller/admin/prices_spa_controller.rb
+++ app/controller/admin/prices_spa_controller.rb
@@ -12,19 +12,6 @@ module Controller
           @title = "Prices SPA page"
           @message = "Select options to load prices"
 
-          # Initialize empty data
-          @rental_locations = []
-          @rate_types = []
-          @season_definitions = []
-          @seasons = []
-          @price_periods = []
-          @prices = []
-          @selected_rental_location = ''
-          @selected_rate_type = ''
-          @selected_season_definition = ''
-          @selected_season = ''
-          @selected_duration = ''
-
           erb :prices_spa
         end
 
